2f450969c3f995c9360436c51f28a12fea6503cd
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-04-12 19:23:42 -0600

    adding firebase link and getting firebase to properly store data

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+//firebase link
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBmm8rkX10XUZPJMwLU62BcKZpxxZCeAZU",
+    authDomain: "train-scheduler-32c9d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-32c9d.firebaseio.com",
+    projectId: "train-scheduler-32c9d",
+    storageBucket: "train-scheduler-32c9d.appspot.com",
+    messagingSenderId: "1082824460664",
+    appId: "1:1082824460664:web:e329e18c320d87dddba08d",
+    measurementId: "G-CZHSZ8TB70"
+};
+
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ //link up firebase with js
+
+ var database = firebase.database();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
